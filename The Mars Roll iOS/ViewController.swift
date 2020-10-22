66c353434ef93b9f28578542b40af59a8ffd2b7b
Ryan Bennett <ryanbennett@Ryans-MacBook-Pro.local>
2020-10-22 10:25:13 -0500

    initial commit - pull gitignore and push starting files

---
+++ The Mars Roll iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Mars Roll iOS
+//
+//  Created by Ryan Bennett on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ The Mars Roll iOSTests/The_Mars_Roll_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  The_Mars_Roll_iOSTests.swift
+//  The Mars Roll iOSTests
+//
+//  Created by Ryan Bennett on 10/22/20.
+//
+
+import XCTest
+@testable import The_Mars_Roll_iOS
+
+class The_Mars_Roll_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
